05b59200a75d08b67f0aff3c38a8988dd12d6e57
hvdwolf <hvdwolf@gmail.com>
2016-05-25 21:59:33 +0200

    Update full_addres_map.sh
    Make script slightly simpler

--- full_addres_map.sh
+++ full_addres_map.sh
@@ -19,12 +19,11 @@ echo $CURDIR
 # Specify country as option to this script
 if [ "$#" -ne "1" ] ; then
         echo "Syntax: $0 country"
-        echo "With country being one of: France Germany Italy (\"small\" countries are possible but don't make sense)"
+        echo "With country being one of: France Germany Italy"
         exit 0
 fi
 
 # Note: Some countries do have a full map and  regional maps. In that case it is not usefull to use this script
-#listing=$(curl -s $URL | awk '{print $6}' | sed -e 's+href="++' -e 's+".*++' | grep ${1}.*road | grep -v ${1}_europe)
 listing=$(curl -s $URL | awk '{print $6}' | sed -e 's+href="++' -e 's+".*++' | grep ${1}.*road)
 
 
@@ -40,12 +39,7 @@ done
 cd ${OMC}
 echo ${OMC}
 
-if [ -z "$JAVA_OPTS" ]; then 
-        JAVA_OPTS="-Xms64M -Xmx3512M"
-fi
-
-
-java -Djava.util.logging.config.file=logging.properties $JAVA_OPTS -cp "$OMC/OsmAndMapCreator.jar:$OMC/lib/OsmAnd-core.jar:$OMC/lib/*.jar" net.osmand.MainUtilities merge-address-index ${CURDIR}/${1}.obf ${CURDIR}/${1}*.obf
+java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx6144M -cp "$OMC/OsmAndMapCreator.jar:$OMC/lib/OsmAnd-core.jar:$OMC/lib/*.jar" net.osmand.MainUtilities merge-address-index ${CURDIR}/${1}.obf ${CURDIR}/${1}*.obf
 
 ADDRESS_INDEX="$($INSP ${CURDIR}/${1}.obf 2>&1 | grep "Address data" | awk '{print $1}')"
 echo $ADDRESS_INDEX
